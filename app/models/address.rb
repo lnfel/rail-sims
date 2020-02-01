2166c37d62709637fe34d35af303a791f2c8f767
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2020-02-02 07:36:55 +0800

    Modify validates to validate

--- app/models/address.rb
+++ app/models/address.rb
@@ -6,22 +6,22 @@ class Address < ApplicationRecord
   belongs_to :modified_by_account_type
   belongs_to :modified_by
 
-  validates :house_number, allow_nil: true
-  validates :block, allow_nil: true
-  validates :phase, allow_nil: true
-  validates :building_number, allow_nil: true
-  validates :building_name, allow_nil: true
-  validates :floor_number, allow_nil: true
-  validates :room_number, allow_nil: true
-  validates :street_name, allow_nil: true
-  validates :subdivision, allow_nil: true
-  validates :compound, allow_nil: true
-  validates :district, allow_nil: true
-  validates :municipality, allow_nil: true
-  validates :city, allow_nil: true
-  validates :region, allow_nil: true
-  validates :country, allow_nil: true
-  validates :zip_code, allow_nil: true
-  validates :modified_by_account_type, allow_nil: true
-  validates :modified_by, allow_nil: true
+  validate :house_number, allow_nil: true
+  validate :block, allow_nil: true
+  validate :phase, allow_nil: true
+  validate :building_number, allow_nil: true
+  validate :building_name, allow_nil: true
+  validate :floor_number, allow_nil: true
+  validate :room_number, allow_nil: true
+  validate :street_name, allow_nil: true
+  validate :subdivision, allow_nil: true
+  validate :compound, allow_nil: true
+  validate :district, allow_nil: true
+  validate :municipality, allow_nil: true
+  validate :city, allow_nil: true
+  validate :region, allow_nil: true
+  validate :country, allow_nil: true
+  validate :zip_code, allow_nil: true
+  validate :modified_by_account_type, allow_nil: true
+  validate :modified_by, allow_nil: true
 end
